fa588025e05b298b017f190f5b7279458780b0b1
unknown <gabrielvelox10@hotmail.com>
2023-06-14 22:54:40 -0300

    tentativa numero 84

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import './styles.css'
+
+function App() {
+  const urlToDownload = 'http://speed.hetzner.de/100MB.bin'
+  const [download, setDownload] = useState('')
+  const [count, setCount] = useState(0)
+  return (
+    <div className="App">  
+      <h1 className="title">
+         tentativa numero 84
+      </h1>
+      <div className="container">
+      <div   className= "btn">
+        <button  onClick=
+        {()=> {
+         setDownload(urlToDownload);
+         setCount(oud => oud + 1);
+        }}>Download
+        </button>
+      </div>
+      </div>
+      <p>{download}</p>
+      <div className="container-iframe">
+        {download && <iframe src= {download +'?c='+count}></iframe>}
+      </div>
+    </div>
+  );
+}
+
+export default App;
